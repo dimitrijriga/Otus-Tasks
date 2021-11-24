30b98b1b38a655c2a59e13ee2c738c147c5ff72d
Dmitry Zakomolkin <dimitrijriga@gmail.com>
2021-11-24 21:22:09 +0200

    NFS v3

--- 1_5_nfs-fuse/init_srv.sh
+++ 1_5_nfs-fuse/init_srv.sh
@@ -10,12 +10,28 @@ SELINUXTYPE=targeted
 __EOF
 
 sudo -i
+systemctl start firewalld
+firewall-cmd --permanent --add-service=nfs --zone=public
+firewall-cmd --permanent --add-service=rpc-bind --zone=public
+firewall-cmd --permanent --add-service=mountd --zone=public
+firewall-cmd --reload
+
 yum install nfs-utils -y
+yum install -y net-tools
+
 mkdir /tmp/nfs_share
 mkdir /tmp/nfs_share/upload
+
+echo "/tmp/nfs_share 192.168.56.11(rw,sync,no_root_squash)" > /etc/exports
+sed -i 's/\# vers3=n/vers3=y/g' /etc/nfs.conf
+sed -i 's/\# vers4=y/vers4=n/g' /etc/nfs.conf
+sed -i 's/\# vers4.0=y/vers4.0=n/g' /etc/nfs.conf
+sed -i 's/\# vers4.1=y/vers4.1=n/g' /etc/nfs.conf
+sed -i 's/\# vers4.2=y/vers4.2=n/g' /etc/nfs.conf
+sed -i 's/\# tcp=y/udp=y/g' /etc/nfs.conf
+
 systemctl enable nfs-mountd.service
 systemctl start nfs-mountd.service
 
-echo "/tmp/nfs_share 192.168.56.11(rw,sync,no_root_squash)" > /etc/exports
 exportfs -s
 exportfs -a
